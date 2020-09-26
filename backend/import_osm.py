7891e21bb63d7382a421e819ddae7cd423524ab7
Andrew <gavrilov.andrew1999@yandex.ru>
2020-09-26 15:12:09 +0300

    import from mongo(working)

--- backend/import_osm.py
+++ backend/import_osm.py
@@ -1,6 +1,7 @@
-import xml.sax
+from xml.sax import ContentHandler, make_parser
 from pymongo import MongoClient
 from db_types import *
+
 
 def db_clear(db):
     db.nodes.remove()
@@ -8,7 +9,24 @@ def db_clear(db):
     db.relations.remove()
 
 
-class OSMXMLFileParser(xml.sax.ContentHandler):
+def create_db(name):
+    client = MongoClient('localhost', 27017)
+    db = client[name]
+    dbnodes = db.nodes
+    dbways = db.ways
+    dbrelations = db.relations
+    db_clear(db)
+    return db
+
+
+def import_to_bd(filename):
+    db = create_db('map_spb')
+    parser = make_parser()
+    parser.setContentHandler(OSMXMLFileParser(db))
+    parser.parse(filename)
+
+
+class OSMXMLFileParser(ContentHandler):
     def __init__(self, db):
         self.db = db
         self.curr_node = None
@@ -19,13 +37,10 @@ class OSMXMLFileParser(xml.sax.ContentHandler):
         self.relationsCount = 0
 
     def startElement(self, name, attrs):
-        print('START!!!!!!!!!!!')
         if name == 'node':
             self.curr_node = Node(id=int(attrs['id']), lon=float(attrs['lon']), lat=float(attrs['lat']),
                                   visible='true')
-
         elif name == 'way':
-            # self.containing_obj.ways.append(Way())
             self.curr_way = Way(id=int(attrs['id']), visible='true')
 
         elif name == 'relation':
@@ -44,13 +59,11 @@ class OSMXMLFileParser(xml.sax.ContentHandler):
 
         elif name == 'member':
             member = Member(type=attrs['type'], ref=int(attrs['ref']))
-            if attrs.has_key('role') == True:
+            if 'role' in attrs.keys():
                 member.role = attrs['role']
             self.curr_relation.members.append(member)
 
     def endElement(self, name):
-        print('END!!!!!!!!!!!')
-        # assert not self.curr_node and not self.curr_way, "curr_node (%r) and curr_way (%r) are both non-None" % (self.curr_node, self.curr_way)
         if name == 'node':
             dbnode = {'_id': self.curr_node.id,
                       'lon': self.curr_node.lon,
@@ -65,38 +78,15 @@ class OSMXMLFileParser(xml.sax.ContentHandler):
             self.curr_node = None
 
         elif name == 'way':
-            dbway = {'_id': self.curr_way.id,
-                     'visible': self.curr_way.visible,
-                     'in_relations': []}
-            dbway['nodes'] = self.curr_way.nodes
-            dbway['tags'] = self.curr_way.tags
+            dbway = {'_id': self.curr_way.id, 'visible': self.curr_way.visible, 'in_relations': [],
+                     'nodes': self.curr_way.nodes, 'tags': self.curr_way.tags}
             self.db.ways.save(dbway)
             self.curr_way = None
 
         elif name == 'relation':
-            dbrelation = {'_id': self.curr_relation.id,
-                          'in_relations': []}
-            dbrelation['members'] = []
+            dbrelation = {'_id': self.curr_relation.id, 'in_relations': [], 'members': []}
             for member in self.curr_relation.members:
                 dbrelation['members'].append(member.list())
             dbrelation['tags'] = self.curr_relation.tags
             self.db.relations.save(dbrelation)
             self.curr_relation = None
-
-def create_db():
-    client = MongoClient('localhost', 27017)
-    db = client['spb_map']
-    dbnodes = db.nodes
-    dbways = db.ways
-    dbrelations = db.relations
-    return db
-
-def import_to_bd(filename):
-    db = create_db()
-    parser = xml.sax.make_parser()
-    parser.setContentHandler(OSMXMLFileParser(db))
-    parser.parse(filename)
-
-with open("../map") as file:
-    file.close()
-    import_to_bd('../map')

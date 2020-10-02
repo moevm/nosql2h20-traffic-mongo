7985f5e157a270e1d06f82bee6c7324b6a61f6c2
annamenko <yeremenkogrey@gmail.com>
2020-10-03 00:11:09 +0300

    Db getting fixed

--- backend/models/find_path.py
+++ backend/models/find_path.py
@@ -1,9 +1,11 @@
-from models.get_model import get_mongo
+from models.get_model import get_mongo, DB_NAME
 
 # this is mongo!
-db = get_mongo()
+db = get_mongo()[DB_NAME]
+
 
 def find_paths(x0, y0, x1, y1):
+    # TODO
     return []
     # Should returns array of ways
     # request to db | some algortms

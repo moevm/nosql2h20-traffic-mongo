f69e1795020dc34747e41bfe948ad7ceb263a322
Andrew <gavrilov.andrew1999@yandex.ru>
2020-10-18 18:33:51 +0300

    crating correct demo answer

--- backend/models/find_path.py
+++ backend/models/find_path.py
@@ -5,7 +5,10 @@ db = get_mongo()[DB_NAME]
 
 
 def find_paths(x0, y0, x1, y1):
-    # TODO
-    return []
+    nodeid_from = find_nearest_node(x0,y0)
+    return [[x0, y0], [x1, y1], [59.9180, 30.3088]]
     # Should returns array of ways
     # request to db | some algortms
+
+def find_nearest_node(x, y):
+    dbnodes = db.nodes

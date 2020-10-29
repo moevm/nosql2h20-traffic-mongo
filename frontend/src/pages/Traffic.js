65a5044b49fb138e722484e8dc9ea59505d5e3bd
light5551 <sergey.glazunov.99@mail.ru>
2020-10-29 22:16:06 +0300

    chnage frontend

--- frontend/src/pages/Traffic.js
+++ frontend/src/pages/Traffic.js
@@ -1,10 +1,41 @@
 import React from "react";
-import {Table} from "react-bootstrap";
+import {Form, FormControl, InputGroup, Table} from "react-bootstrap";
 import MainContainer from "../containers/MainContainer";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 export default function Traffic() {
     return <>
         <MainContainer>
+          <br/>
+          <h2>Traffic jams levels</h2>
+          <br/>
+          <Form>
+            <Form.Row>
+              <Col>
+                <Form.Control placeholder="Min range (default - 0)" />
+              </Col>
+              <Col>
+                <Form.Control placeholder="Max range  (default - 10)" />
+              </Col>
+            </Form.Row>
+          </Form>
+          <br/>
+          <Row>
+            <h2>Collection <i>Ways</i></h2>
+            <InputGroup className="mb-3">
+              <FormControl
+                  placeholder="Find way by id"
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="primary">search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Row>
+          <br/>
             <Table striped bordered hover>
   <thead>
     <tr>

3ebb7c463bea2bb5bca3758e7b158fdf8df2bb40
annamenko <yeremenkogrey@gmail.com>
2020-11-26 22:34:34 +0300

    Adding traffic update

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from resources.apimap import ApiMap
 from resources.ways import Ways
 from resources.BDworker import BDworker as bd
 from resources.stat import Stat
+from utils.traffic_update import start_updating_thread
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,4 +16,6 @@ api.add_resource(bd, '/bd')
 api.add_resource(Stat, '/stat')
 
 if __name__ == "__main__":
+    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
+        second_thread = start_updating_thread()
     app.run(port=5000, host='0.0.0.0', debug=True)

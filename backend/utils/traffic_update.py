5f6d9ccdf89bff97cd82b9aff52c816e09b8110d
annamenko <yeremenkogrey@gmail.com>
2020-11-26 22:46:26 +0300

    Fixed empty file

---
+++ backend/utils/traffic_update.py
@@ -0,0 +1,45 @@
+import threading
+import time
+import random
+from pymongo import UpdateOne
+from pymongo.errors import BulkWriteError
+from models.get_model import get_mongo, DB_NAME
+
+
+def start_updating_thread():
+    th = threading.Thread(target=update_data)
+    th.start()
+    return th
+
+
+def update_data():
+    time.sleep(5)
+    mongo = get_mongo()
+    db = mongo[DB_NAME]
+    if mongo is None:
+        print("Starting second thread failed")
+        return None
+    count = db.ways.count()
+    print("Second thread started")
+    while True:
+        update_all_at_once(db.ways.find(), db)
+        time.sleep(10)
+
+
+def update_all_at_once(ways, db):
+    try:
+        update_el = [
+            UpdateOne(
+                {'_id': el['_id']},
+                {'$set':
+                     {'avg_speed': random.randint(0, 100)}
+                 }
+            )
+            for el in ways]
+        result = db.ways.bulk_write(update_el)
+        print(f"Values updated, errors: {result.bulk_api_result['writeErrors']}")
+    except BulkWriteError as bwe:
+        print(bwe.details)
+    except BaseException:
+        print("Unrecognized error")
+    time.sleep(5 * 60)
